96853129d5fe6c442b79ca5b236ad67b3f0a9035
ruriazz <azizruri.s@gmail.com>
2022-01-26 02:32:22 +0700

    initial master

--- challengeapi/settings.py
+++ challengeapi/settings.py
@@ -127,4 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 django_heroku.settings(locals())
